e6ac2732d80b75bc8bada83b97c3ab7da589e648
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2023-12-09 14:46:04 +0100

    solution

--- tasks/2023-12-09/index.ts
+++ tasks/2023-12-09/index.ts
@@ -1,1 +1,28 @@
-// Tutaj skopiuj kod zadania
+export interface Tool {
+	init: Function;
+	update: Function;
+	dispose: Function;
+}
+
+export class Equipment {
+	#tools: Tool[] = [];
+	#areToolsInitialized = false;
+
+	registerTools(tool: Tool): void {
+		this.#tools.push(tool);
+	}
+	initializeTools(): void {
+		this.#tools.forEach((t) => t.init());
+		this.#areToolsInitialized = true;
+	}
+	updateTools(): void {
+		if (this.#areToolsInitialized) {
+			this.#tools.forEach((t) => t.update());
+		} else {
+			throw Error('Cannot update any tools before initialization.');
+		}
+	}
+	disposeTools(): void {
+		this.#tools.forEach((t) => t.dispose());
+	}
+}
