4b8c24d1e904b2fe10a46aecd3ffb2fb2f9eea1f
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2023-12-18 21:08:29 +0100

    solution

--- tasks/2023-12-18/index.ts
+++ tasks/2023-12-18/index.ts
@@ -1,1 +1,25 @@
-export class RateLimiter {}
+export class RateLimiter {
+	#maxRequests: number;
+	#intervalMs: number;
+	#pendingRequests: boolean[] = [];
+
+	constructor(maxRequests: number, intervalMs: number) {
+		this.#maxRequests = maxRequests;
+		this.#intervalMs = intervalMs;
+	}
+
+	#addRequest() {
+		this.#pendingRequests.push(true);
+		setTimeout(() => {
+			this.#pendingRequests.shift();
+		}, this.#intervalMs);
+	}
+
+	attemptAccess(): boolean {
+		if (this.#pendingRequests.length >= this.#maxRequests) {
+			return false;
+		}
+		this.#addRequest();
+		return true;
+	}
+}
