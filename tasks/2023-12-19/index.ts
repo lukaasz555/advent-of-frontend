dda96fc76e63d75fdd96bf78fdbda52d15a0170d
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2023-12-19 20:05:50 +0100

    solution

--- tasks/2023-12-19/index.ts
+++ tasks/2023-12-19/index.ts
@@ -2,4 +2,15 @@ export function usePagination<T>(
 	items: T[],
 	itemsPerPage: number,
 	pageNumber: number
-) {}
+) {
+	const totalItems = items.length;
+	const totalPages = Math.ceil(items.length / itemsPerPage);
+	const offset = (pageNumber - 1) * itemsPerPage;
+	const currentPageItems = items.splice(offset, itemsPerPage);
+
+	return {
+		totalPages,
+		totalItems,
+		currentPageItems,
+	};
+}
